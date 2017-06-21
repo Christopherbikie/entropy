a19d9bfa7453d95cecbe30f6742f7e4ab1609e41
Christopherbikie <christopher.hall.3609@gmail.com>
2017-06-21 20:40:42 +1000

    Clean up GDT code

--- kernel/arch/i386/gdt.c
+++ kernel/arch/i386/gdt.c
@@ -1,45 +1,75 @@
+#include <stdint.h>
+
 #include "kernel/gdt.h"
 
-struct gdt_entry gdt[3];
+#define SEGMENT_BASE    0
+#define SEGMENT_LIMIT   0xFFFFF
 
-static struct gdt_entry make_desc(uint32_t base, uint32_t limit, enum seg_privilege dpl, enum seg_executable exec, enum seg_granularity gran) 
+#define CODE_RX_TYPE    0xA
+#define DATA_RW_TYPE    0x2
+
+#define GDT_NUM_ENTRIES 5
+
+enum seg_privilege
+{
+	PRIV_KERNEL = 0, // Kernel mode (ring 0)
+	PRIV_USER   = 3  // User mode (ring 3)
+};
+
+struct gdt_entry
+{
+	uint16_t limit_low;
+	uint16_t base_low;
+	uint8_t base_mid;
+	uint8_t access;
+	uint8_t limit_high_flags;
+	uint8_t base_high;
+} __attribute__((packed));
+
+struct gdt_ptr
+{
+	uint16_t limit;
+	uint32_t base;
+} __attribute__((packed));
+
+struct gdt_entry gdt[GDT_NUM_ENTRIES];
+
+void gdt_load_and_set(uint32_t gdtr);
+
+static struct gdt_entry make_desc(enum seg_privilege dpl, uint8_t type)
 {
 	struct gdt_entry desc;
 
-	desc.limit_low = limit & 0xFFFF;       // Limit 0:15
-	desc.base_low = base & 0xFFFF;         // Base 0:15
-	desc.base_mid = (limit >> 16) & 0xFF;  // Base 16:23
-	desc.access =                          // Access byte
-		(1 << 7)                              // Present bit
-		| ((dpl & 0x7) << 5)                  // Description Privilege Level
-		| (1 << 4)                            // 1
-		| (exec << 3)                         // Executable bit
-		| (0 << 2)                            // Direction/conforming bit
-		| (1 << 1)                            // Read/write bit
-		| (0 << 0);                           // Accessed bit
-	desc.limit_high_flags =                // Flags and limit 16:19
-		(gran << 7)                           // Granularity
-		| (1 << 6)                            // Size bit (16/32)
-		| (0 << 5)                            // Reserved (not x86-64)
-		| (0 << 4)                            // Reserved
-		| ((limit >> 16) & 0xFF);             // limit 16:19
-	desc.base_high = (base >> 24) & 0xFF;  // Base 24:31
-
+	desc.limit_low = SEGMENT_LIMIT & 0xFFFF;      // Limit 0:15
+	desc.base_low = SEGMENT_BASE & 0xFFFF;        // Base 0:15
+	desc.base_mid = (SEGMENT_BASE >> 16) & 0xFF;  // Base 16:23
+	desc.access =                                 // Access byte
+		(0x01 << 7)                                  // Segment present in memory
+		| ((dpl & 0x03) << 5)                        // Descriptor privilege level
+		| (0x01 << 4)                                // Descriptor type (0 = system, 1 = code or data)
+		| (type & 0x0F);                             // Segment type, describes how it can be accessed
+	desc.limit_high_flags =                       // Limit 16:19 and flags
+		(0x01 << 7)                                  // Granularity (1 = page)
+		| (0x01 << 6)                                // Size of operation (0 = 16 bits, 1 = 32 bits)
+		| (0x00 << 5)                                // 1 = 64 bit code
+		| (0x00 << 4)                                // "available for use by system software"
+		| ((SEGMENT_LIMIT >> 16) & 0x0F);            // Limit 15:19
+	desc.base_high = (SEGMENT_BASE >> 24) & 0xFF; // Base 24:31
+	
 	return desc;
-}
-
-static uint64_t make_gdtr(uint32_t limit, void *base)
-{
-	return limit | ((uint64_t) (uint32_t) base << 16);
 }
 
 void gdt_init()
 {
-	gdt[0] = (struct gdt_entry) {0, 0, 0, 0, 0, 0};
-	gdt[1] = make_desc(0, 0xFFFFFFFF, 0, EXEC_CODE, GRAN_PAGE);
-	gdt[2] = make_desc(0, 0xFFFFFFFF, 0, EXEC_DATA, GRAN_PAGE);
+	gdt[0] = make_desc(0, 0);
+	gdt[1] = make_desc(PRIV_KERNEL, CODE_RX_TYPE);
+	gdt[2] = make_desc(PRIV_KERNEL, DATA_RW_TYPE);
+	gdt[3] = make_desc(PRIV_USER, CODE_RX_TYPE);
+	gdt[4] = make_desc(PRIV_USER, DATA_RW_TYPE);
 
-	uint32_t gdtr = make_gdtr(sizeof(gdt) - 1, gdt);
-	asm volatile("lgdt %0" : :"m" (gdtr));
+	struct gdt_ptr gdt_ptr;
+    gdt_ptr.limit = sizeof(struct gdt_entry) * GDT_NUM_ENTRIES;
+    gdt_ptr.base = (uint32_t) &gdt;
+	
+	gdt_load_and_set((uint32_t) &gdt_ptr);
 }
-
--- kernel/include/kernel/gdt.h
+++ kernel/include/kernel/gdt.h
@@ -1,43 +1,7 @@
-#include <stdint.h>
-
 #ifdef __cplusplus
 extern "C"
 {
 #endif
-
-enum seg_executable
-{
-	EXEC_DATA = 0, // Data segment
-	EXEC_CODE = 1  // Code segment
-};
-
-enum seg_granularity
-{
-	GRAN_BYTE = 0, // 1 byte granularity
-	GRAN_PAGE = 1  // 4 kB granualrity
-};
-
-enum seg_privilege
-{
-	PRIV_KERNEL = 0, // Kernel mode (ring 0)
-	PRIV_USR    = 3  // User mode (ring 3)
-};
-
-struct gdt_entry
-{
-	uint16_t limit_low;
-	uint16_t base_low;
-	uint8_t base_mid;
-	uint8_t access;
-	uint8_t limit_high_flags;
-	uint8_t base_high;
-} __attribute__((packed));
-
-struct gdt_ptr
-{
-	uint16_t limit;
-	uint32_t base;
-} __attribute__((packed));
 
 void gdt_init();
 
