6c75132cefa61f77721b06cab7121de87b3bbcf3
Christopherbikie <christopher.hall.3609@gmail.com>
2017-06-26 23:01:17 +1000

    libc: add missing header and TODO to assert.h

--- libc/assert/assert.c
+++ libc/assert/assert.c
@@ -1,8 +1,10 @@
 #include <assert.h>
+#include <stdio.h>
 
 void __assert(const char *file, int line, const char *func, const char *expr)
 {
 	// TODO: print to stderr
+	// TODO: print line number properly
 	printf("Assertion failed: %s:%s: %s: \"%s\"", file, line, func, expr);
 }
 
