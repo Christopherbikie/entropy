d7941a2eea1fd260d2b407a8a9852728a18f0704
Christopherbikie <christopher.hall.3609@gmail.com>
2017-06-26 22:54:14 +1000

    libc: Add strcmp

--- libc/include/string.h
+++ libc/include/string.h
@@ -13,7 +13,9 @@ int memcmp(const void *aptr, const void *bptr, size_t size);
 void* memcpy(void *dstptr, const void *srcptr, size_t size);
 void* memmove(void *dstptr, const void *srcptr, size_t size);
 void* memset(void *bufptr, int value, size_t size);
+
 long unsigned int strlen(const char *str);
+int strcmp(const char *str1, const char *str2);
 
 #ifdef __cplusplus
 }
